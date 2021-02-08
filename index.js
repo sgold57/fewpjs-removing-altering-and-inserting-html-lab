600883c44f50b8509102a4cf7ad59193ae7f6a5e
Sam Gold <samg915757@gmail.com>
2021-02-07 18:59:56 -0700

    commit tree 477bf7be8d0ab035edcd2fa13d9b23b3c66537d0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const nodeToRemove = document.getElementById('main')
+nodeToRemove.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "SAM is the champion"
+document.body.append(newHeader)
+
